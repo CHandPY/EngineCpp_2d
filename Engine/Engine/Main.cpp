19a44661baa8080c7cef87b5da4004cd2e1cd401
Claude-Rochenoir Rochenoir <chrochenoir@gmail.com>
2015-12-19 00:36:39 +0100

    can u tell im bored of typing these commit messages?

--- Engine/Engine/Main.cpp
+++ Engine/Engine/Main.cpp
@@ -7,6 +7,7 @@ int main() {
 	cout << "Hello World" << endl;
 	cout << "Hello again, World" << endl;
 	cout << "Hello World... yeah... its the third time..." << endl;
+	cout << "Sory..." << endl;
 
 	system("pause");
 
